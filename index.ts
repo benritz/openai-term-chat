24ed31284846b405589d5750e40cd1c6eda4aa18
benritz <23510442+benritz@users.noreply.github.com>
2023-11-10 07:55:44 +0000

    Pre Ink

--- index.ts
+++ index.ts
@@ -8,23 +8,54 @@ const openai = new OpenAI({
     apiKey: process.env.API_KEY
 })
 
-const ui = readline.createInterface({
+const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {role: 'system', content: 'You are not very helpful'}
+]
+
+const model = 'gpt-3.5-turbo'
+
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
+    prompt: 'You: '
 })
 
-ui.prompt()
+process.stdout.write(`Welcome to OpenAI terminal chat. Enter a prompt to begin chatting. Ctrl-D to quit.\n`)
 
-ui.on('line', async (content) => {
+rl.prompt()
+
+rl.on('line', async (line) => {
+    messages.push({role: 'user', content: line})
+
     const ret = await openai.chat.completions.create({
-        messages: [
-            {role: 'user', content}
-        ],
-        model: 'gpt-3.5-turbo'
+        messages,
+        model
     })
     
     console.log(ret)
-    
-    console.log(ret.choices[0])
+
+
+    const outputChoice = (choice: OpenAI.Chat.Completions.ChatCompletion.Choice) => {
+        const {message} = choice
+
+        messages.push(message)
+        const {content} = message
+        if (content) {
+            process.stdout.write(`Assistent: ${content}\n`)
+        }    
+    }
+
+    const {choices} = ret
+
+    if (choices.length === 0) {
+        // what?
+    } else if (choices.length === 1) {
+        outputChoice(choices[0])
+    } else {
+        outputChoice(choices[0])
+        // display multiple choices and options to switch between them
+    }
+
+    rl.prompt()
 })
 
